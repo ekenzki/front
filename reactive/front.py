d5cd4ff83c740d4dd33d2408e67d9f8472328fbd
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-05-31 14:29:40 +0300

    Add scania train interface

--- reactive/front.py
+++ reactive/front.py
@@ -1,17 +1,15 @@
-from charms.reactive import when, when_not, set_state
+from charmhelpers.core.hookenv import status_set, log
+from charms.reactive import when, when_not, set_state, endpoint_from_flag
 
+@when_not('endpoint.backend.joined')
+def not_ready_yet():
+    status_set('blocked', 'Missing backend relation')
 
-@when_not('front.installed')
-def install_front():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
-    set_state('front.installed')
+@when('endpoint.backend.new-signal')
+def is_backend_connected():
+    log("We got a signal from the backend")
+    backend = endpoint_from_flag('endpoint.backend.new-signal')
+    if backend.are_backends_ready():
+        status_set('active', 'Ready')
+    else:
+        status_set('waiting', 'Waiting for backends to come online')
